f36ee51979a8aa1b69c3cbeb0fe724df3c338a5d
Vleezy <vladdromero@gmail.com>
2021-03-09 17:32:54 -0500

    Adding React-Router + Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+
+// PAGES
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 
 
 function App() {
   return (
+
+    <Router>
     <div className="container">
-        <p>Will begin to add stuff!</p>    
+        <p>Will begin to add stuff!  -Sungho & Vlad</p>    
     </div>
+
+{/* NAVBAR */}
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+         <Link to="/about">About</Link>
+        </li>
+        <li>
+         <Link to="/Home">Home</Link>
+        </li>
+      </ul>
+    </nav>
+
+    {/* REACT NAV  */}
+  <Switch>
+    <Route path="/" exact component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/contact" component={Contact}/>
+  </Switch>
+
+    </Router>
   );
 }
 
