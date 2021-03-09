8951c7c1271500dbb259a8d2a466c01993c921a2
Vleezy <vladdromero@gmail.com>
2021-03-09 17:12:26 -0500

    Clean

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button className="Sungho">Park
-        </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-<<<<<<< HEAD
-          BLAHBLAHBLAH TEST!@#
-        </a>
-=======
-Vlad testing        </a>
->>>>>>> cb8cc3488da979340d34b4dd34a57d076c767ee7
-      </header>
-
+    <div className="container">
+        <p>Will begin to add stuff!</p>    
     </div>
   );
 }
