85e7620dfbda4129c14c51b8d256d75c1b430c88
Vleezy <vladdromero@gmail.com>
2021-03-11 11:20:01 -0500

    Logo  and nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,46 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components/macro'
 import {Link} from 'react-router-dom'
 import { menuData } from '../data/MenuData';
 
-// Styled Components
+// Styled Components (CSS)
 const Nav = styled.nav`
+    display: flex;
     height: 60px;
+    justify-content:space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
     background: teal;
 `;
 
+const NavLink = css`
+color: #fff;
+display: flex;
+align-items: center;
+padding:0 1rem;
+height 100%;
+cursor: pointer;
+text-decoration:none;
+`;
+
 const Logo = styled(Link)`
-    color:#fff;
+    ${NavLink}
+    font-style: italic;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+display:flex;
+align-items:center;
+`;
 
-const NavMenuLinks = styled(Link)``;
+const NavMenuLinks = styled(Link)`
+${NavLink}
+`;
+
 
 export const Navbar = () => {
     return (
@@ -30,16 +53,16 @@ export const Navbar = () => {
             {/* Links being mapped: index.js contains data */}
             <NavMenu>
 
-                {menuData.map((item, index) => {
+                {menuData.map((item, index) => (
 
                     <NavMenuLinks to={item.link} key={index}>
                         {item.title}
                     </NavMenuLinks>
 
-                })}
+    ))}
 
             </NavMenu>
-            <h1> test1 </h1>
+            <h1>Testing</h1>
         </Nav>
     );
 };
